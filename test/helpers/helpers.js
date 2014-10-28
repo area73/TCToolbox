8db3af42c9706ccb224d22f58361d35467528a29
Sergio Ramirez <hr@SergioRamirez.local>
2014-10-28 11:56:19 +0100

    Sticky tests

--- test/helpers/helpers.js
+++ test/helpers/helpers.js
@@ -1,1 +1,30 @@
 // Write your test helpers here
+var StickyHelpers = {
+  _testContainer: function(){
+    this.container = $("<div id='sticky-test-html'>");
+    this.container.prependTo("body");
+  },
+  _testDiv: function(w, h){
+    this.container.append("<div data-sticky-block "+
+                                "style='width:"+w+"px; height:"+h+"px; margin:0 auto;"+
+                                       "background-color: #ccc; color:#969696;"+
+                                       "line-height:"+h+"px; text-align: center;"+
+                                       "font-size: 20px; font-weight:bold;"+
+                                       "font-family:Arial, Helvetica, sans-serif;'>"+
+      w+"x"+h+
+    "</div>")
+  },
+  testHTML: function(){
+    $("body, html").css({
+      margin: 0,
+      padding: 0
+    })
+    this._testContainer();
+    this._testDiv(400,200);
+    this._testDiv(400,400);
+    this._testDiv(400,600);
+    this._testDiv(400,300);
+    this._testDiv(400,500);
+  }
+  
+}
--- test/specs/Sticky.spec.js
+++ test/specs/Sticky.spec.js
@@ -1,10 +1,77 @@
 describe("Sticky", function(){
+  StickyHelpers.testHTML();
+  $("[data-sticky-block]").each(function(){
+    TCT.Sticky.instance()
+      .register($(this))
+  })
+  var instance = TCT.Sticky.instance();
   it("should be a singleton", function(){
-    expect(TCT.Sticky.instance()).toBe(TCT.Sticky._instance);
+    expect(instance).toBe(TCT.Sticky._instance);
   }); 
   it("should fail when calling new TCT.Sticky()", function(){
     expect(function(){
       new TCT.Sticky()
     }).toThrow(new Error("TCT.Sticky is a singleton. Please use TCT.Sticky.instance()"));
-  }); 
+  });
+  describe("when running", function(){
+    it("should fire the on_scroll callback when scrolling", function(done){
+      spyOn(instance, "on_scroll").and.callThrough();
+      instance.start();
+      window.scrollTo(0, 100)
+      setTimeout(function(){        
+        expect(instance.on_scroll).toHaveBeenCalled();
+        instance.stop();
+        done();
+      }, 100);
+    })
+    it("should fire the on_resize callback when resizing the page", function(done){
+      spyOn(instance, "on_resize").and.callThrough();
+      instance.start();
+      $(window).trigger("resize")
+      setTimeout(function(){        
+        expect(instance.on_resize).toHaveBeenCalled();
+        instance.stop();
+        done();
+      }, 100);
+    })
+  });
+  describe("when not running", function(){
+    it("should fire the on_scroll callback when scrolling", function(){
+      spyOn(instance, "on_scroll").and.callThrough();
+      window.scrollTo(0, 100)
+      expect(instance.on_scroll).not.toHaveBeenCalled();
+    })
+    it("should fire the on_resize callback when resizing the page", function(){
+      spyOn(instance, "on_resize").and.callThrough();
+      $(window).trigger("resize")
+      expect(instance.on_resize).not.toHaveBeenCalled();
+    })
+  });
+  describe("when starting", function(){
+    beforeEach(function(done){
+      setTimeout(function(){
+        window.scrollTo(0, 201)
+        done();
+      },100)
+    })
+    it("should trigger a 'sticked' for all sticked elements", function(){
+      spyOnEvent("[data-sticky-block]:nth(0)", "sticked")
+      spyOnEvent("[data-sticky-block]:nth(1)", "sticked")
+      TCT.Sticky.instance().start();
+      expect("sticked").toHaveBeenTriggeredOn("[data-sticky-block]:nth(0)");
+      expect("sticked").toHaveBeenTriggeredOn("[data-sticky-block]:nth(1)");
+      TCT.Sticky.instance().stop();
+    });
+    it("should not trigger a 'sticked' for not sticked elements", function(){
+      spyOnEvent("[data-sticky-block]:nth(2)", "sticked")
+      spyOnEvent("[data-sticky-block]:nth(3)", "sticked")
+      TCT.Sticky.instance().start();
+      expect("sticked").not.toHaveBeenTriggeredOn("[data-sticky-block]:nth(2)");
+      expect("sticked").not.toHaveBeenTriggeredOn("[data-sticky-block]:nth(3)");
+      TCT.Sticky.instance().stop(); 
+    });
+  });
+  describe("when running", function(){
+
+  })
 });

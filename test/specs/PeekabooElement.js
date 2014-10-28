4ad86d00bc02c63aa6a20c22188e7762196903f5
Sergio Ramirez <hr@SergioRamirez.local>
2014-10-28 09:24:07 +0100

    Deleted PeekabooElement.spec

--- test/specs/PeekabooElement.js
+++
@@ -1,6 +0,0 @@
-describe("A PeekabooElement", function(){
-  // it("should give each element an uid", function(){
-  //   TCT.Peekaboo.register(element);
-  //   expect(element.data("peekaboo_id")).toBeDefined();
-  // })
-})
